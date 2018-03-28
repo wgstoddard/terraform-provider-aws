407ad337bb25674595b896256b7e4cbcfa55a158
Bill <stoddard@us.ibm.com>
2018-03-28 18:42:47 +0000

    Update ssh key variable names

--- examples/two-tier/variables.tf
+++ examples/two-tier/variables.tf
@@ -1,11 +1,5 @@
-variable "public_key_path" {
-  description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
-Ensure this keypair is added to your local SSH agent so provisioners can
-connect.
-
-Example: ~/.ssh/terraform.pub
-DESCRIPTION
+variable "public_key" {
+  description = "SSH Public Key"
 }
 
 variable "key_name" {
